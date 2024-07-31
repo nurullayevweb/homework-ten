96552a188ea0a064aa7813ecf593d83733be260e
nurullayevweb <nurullayevweb@gmail.com>
2024-07-31 17:55:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// homework 1
+
+
+// var numbers = [3.02, -3.65, 5, -9];
+
+// function normNum(array){
+//     var newArr=[];
+    
+//     for (var num of array) {
+//         newArr.push(Math.abs(Math.round(num)))
+//     }
+//     return newArr
+// }
+// console.log(normNum(numbers));
+
+
+
+
+// homework 2
+
+// var numbers = [1, 3.2, -5, 8.7];
+
+// function normNum(array){
+//     var result=0;
+    
+//     for (var num of array) {
+//         var newNumbers = Math.abs(Math.round(num));
+//         result +=newNumbers;
+//     }
+//     return result;
+// }
+// console.log(normNum(numbers));
+
+
+
+
+// homework 3
+
+// function findLargeText(txt) {
+//     var textArray = txt.split(" ");
+//     var bigText = textArray[0];
+  
+//     for (var text of textArray) {
+//       if (text.length > bigText.length) {
+//         bigText = text;
+//       }
+//     }
+//     return console.log(bigText);
+//   }
+//   findLargeText("The most wanted free SVG user interface icons");
